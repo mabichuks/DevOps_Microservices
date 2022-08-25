6b53df3b359e4d6ac3da9ff25dd58fda8e58038b
EC2 Default User <ec2-user@ip-172-31-39-87.ec2.internal>
2022-08-25 05:15:01 +0000

    update

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=chukwumamabi/project-ml-microservice-kubernetes:v1.0.0
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag mabi/udacity-docker-devops  $dockerpath
+docker images
+docker login
 
 # Step 3:
-# Push image to a docker repository
+docker push $dockerpath
